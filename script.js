211c74a393508d79763a3041df83ec455c554f97
Frank <francescofontana03@gmail.com>
2026-06-20 00:09:06 +0200

    Update script.js
    cambiato il link che QRcode.js mi trasforma in QRcode

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 window.onload = function() {
-    var urlToEncode = window.location.href; 
+    var urlToEncode = https://frank16super.github.io/PROJECT-WORK-PEGASO/docs/documento_esplicativo_fattore_di_rischio-Francesco-Fontana.pdf; 
     var qrcode = new QRCode(document.getElementById("qrcode"), {
         text: urlToEncode,
-        width: 140, // Larghezza in pixel
-        height: 140, // Altezza in pixel
-        colorDark : "#2c3e50", // Colore dei quadratini
-        colorLight : "#ffffff", // Colore dello sfondo
+        width: 140,
+        height: 140,
+        colorDark : "#2c3e50",
+        colorLight : "#ffffff",
         correctLevel : QRCode.CorrectLevel.H
     });
 };
