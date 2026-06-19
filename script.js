04c4ae7cbdc5c409b87a165465164627ae602be2
Frank <francescofontana03@gmail.com>
2026-06-20 00:30:50 +0200

    Fix URL encoding in QR code generation

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 window.onload = function() {
-    var urlToEncode = "https://frank16super.github.io/PROJECT-WORK-PEGASO/docs/documento_esplicativo_fattore_di_rischio-Francesco-Fontana.pdf";
     var qrcode = new QRCode(document.getElementById("qrcode"), {
-        text: urlToEncode,
+        text: "https://frank16super.github.io/PROJECT-WORK-PEGASO/docs/documento_esplicativo_fattore_di_rischio-Francesco-Fontana.pdf",
         width: 140,
         height: 140,
         colorDark : "#2c3e50",
